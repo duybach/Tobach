2e9c96a1ddaea25064fbf4691dced8534b5f4200
duybach <duybach@live.de>
2019-06-17 02:48:11 +0200

    Remove tutorial first page

--- app/src/main/java/com/example/tobach/Main2Activity.java
+++ app/src/main/java/com/example/tobach/Main2Activity.java
@@ -1,86 +1,47 @@
 package com.example.tobach;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 
 public class Main2Activity extends AppCompatActivity {
-    private Toolbar mToolbar;
     private List<String> playerList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.PLAYER_LIST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        mToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+        Intent intent = getIntent();
+        playerList = intent.getStringArrayListExtra("playerList");
 
-        playerList.add("Toby");
-        playerList.add("Bach");
-        playerList.add("Test user 0");
-        playerList.add("Test user 1");
-        playerList.add("Test user 2");
-        playerList.add("Test user 3");
-
-        recyclerView = findViewById(R.id.my_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new UserAdapter(playerList);
-        recyclerView.setAdapter(mAdapter);
+        setRandomText();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.lobby_menu, menu);
-        return true;
+    public String getRandomPlayer() {
+        Random rand = new Random();
+        return playerList.get(rand.nextInt(playerList.size()));
     }
 
-    /** Called when the user taps the Send button */
-    public void addUser(View view) {
-        EditText editText = (EditText) findViewById(R.id.edit_user);
-        String user = editText.getText().toString();
-        if (!(user.equals(""))) {
-            playerList.add(user);
-            editText.getText().clear();
-
-            mAdapter.notifyDataSetChanged();
-        } else {
-            editText.setError("Name fehlt!");
-        }
-
-        recyclerView.scrollToPosition(playerList.size() - 1);
+    public String getRandomText() {
+        String[] gameTextList = getResources().getStringArray(R.array.game_text_list);
+        Random rand = new Random();
+        return gameTextList[rand.nextInt(gameTextList.length)];
     }
 
-    public void startGame(MenuItem item) {
-        Intent intend = new Intent(this, Main3Activity.class);
-        intend.putStringArrayListExtra("playerList", new ArrayList<>(playerList));
-        startActivity(intend);
+    public void setRandomTextView(View view) {
+        setRandomText();
+    }
+
+    public void setRandomText() {
+        TextView game_view = findViewById(R.id.game_view);
+        game_view.setText(String.format(getRandomText(), getRandomPlayer()));
     }
 }
--- app/src/main/java/com/example/tobach/MainActivity.java
+++ app/src/main/java/com/example/tobach/MainActivity.java
@@ -1,31 +1,86 @@
 package com.example.tobach;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private Toolbar mToolbar;
+    private List<String> playerList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.PLAYER_LIST";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        playerList.add("Toby");
+        playerList.add("Bach");
+        playerList.add("Test user 0");
+        playerList.add("Test user 1");
+        playerList.add("Test user 2");
+        playerList.add("Test user 3");
+
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new UserAdapter(playerList);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.lobby_menu, menu);
+        return true;
     }
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void addUser(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_user);
+        String user = editText.getText().toString();
+        if (!(user.equals(""))) {
+            playerList.add(user);
+            editText.getText().clear();
+
+            mAdapter.notifyDataSetChanged();
+        } else {
+            editText.setError("Name fehlt!");
+        }
+
+        recyclerView.scrollToPosition(playerList.size() - 1);
     }
 
-    public void goToApp(View view) {
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
+    public void startGame(MenuItem item) {
+        Intent intend = new Intent(this, Main2Activity.class);
+        intend.putStringArrayListExtra("playerList", new ArrayList<>(playerList));
+        startActivity(intend);
     }
 }
